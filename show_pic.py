888d98f8a62a4436fc2ccb025c20df9050545415
ggccc <gu199512@qq.com>
2017-02-14 11:26:11 +0800

    Update show_pic.py

--- show_pic.py
+++ show_pic.py
@@ -2,24 +2,37 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import time
+import threading
 
 # http://effbot.org/tkinterbook/canvas.htm#performance-issues
 # https://zhidao.baidu.com/question/752877254572611924.html
 import os
 print(os.getcwd())
-os.chdir(r"C:\Users\1234\Desktop")
-top = Tk()
-for item in os.listdir():
-    print(item[-3:])
+os.chdir(r"F:\我的图片")
+item = os.listdir()
 
-    if item[-3:] != 'jpg':
-        continue
-    im = ImageTk.PhotoImage(Image.open(item))
-    height = im.height()
-    width = im.width()
-    ca = Canvas(top, width = width, height = height)
-    ca.create_image(0, 0, anchor = NW,  image = im)
-    ca.pack()
-    time.sleep(2)
-    mainloop()
-    top = Tk()
+def mainfunc():
+    for i in item:
+        print(i)
+        try:
+            
+            im = Image.open(i)
+            top = Tk()
+            im = ImageTk.PhotoImage(im)           
+            # ca = Label(top, image = im, textvariable = i)
+            ca = Canvas(top,height = im.height(), width = im.width())
+            ca.create_image(0, 0, anchor = NW,  image = im)
+            ca.pack()
+            # time.sleep(2)
+            top.after(1000, lambda : top.destroy())
+            top.mainloop()
+            # top.destroy()
+            
+            
+        except OSError as err:
+            # print(err)
+            continue
+threading.Thread(target=mainfunc, name = 'Thread1').start()
+
+
+    
